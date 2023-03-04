456700e558a4e76db77d873e81de8a200fb8cf0a
Garrett Comes <gcomes3@gatech.edu>
2023-03-03 23:40:16 -0500

    Command Mode

--- core.py
+++ core.py
@@ -4,6 +4,7 @@ from io import BytesIO
 from pydub import AudioSegment
 from pydub.playback import play
 import whisper
+import sys
 
 def setup_microphone():
     r = sr.Recognizer()
@@ -16,27 +17,20 @@ def setup_microphone():
     #r.non_speaking_duration = 0.5
     return r
 
-model = whisper.load_model("base.en")
+model = whisper.load_model("small.en")
 
-def listen_and_recognize_whisper(r: sr.Recognizer, from_code: str):
-    with sr.Microphone() as source:
-        print("Listening...")
-        audio = r.listen(source)
-        with open("temp.wav", "wb") as f:
-            f.write(audio.get_wav_data())
-            f.flush()
-            print("Recognizing...")
-            result = model.transcribe(f.name)
-            print(result['text'])
-
-def recognize_whisper_from_file(r: sr.Recognizer, file_name: str, from_code: str):
-    with sr.AudioFile(file_name) as source:
-        audio = r.record(source)
-        with open("temp.wav", "wb") as f:
-            f.write(audio.get_wav_data())
-            f.flush()
-            result = model.transcribe(f.name)
-            print(result['text'])
+def listen_and_recognize_whisper(r: sr.Recognizer, source, from_code: str, beep=False, timeout=5):
+    if beep:
+        afile = AudioSegment.from_wav("beep.wav")
+        play(afile)
+    print("Listening...")
+    audio = r.listen(source)
+    with open("temp.wav", "wb") as f:
+        f.write(audio.get_wav_data())
+        f.flush()
+        print("Recognizing...")
+        result = model.transcribe(f.name)
+        return result['text']
 
 def say(text, to_code):
     tts = gtts.gTTS(text, lang=to_code)
@@ -46,14 +40,38 @@ def say(text, to_code):
     aud = AudioSegment.from_file(fp, format="mp3")
     play(aud)
 
-def main():
+def recognize_once(r: sr.Recognizer, source):
     print("Starting...")
     r = setup_microphone()
-    while True:
-        text = listen_and_recognize_whisper(r, "en")
-        if text:
-            print("Spoken: ", text)
-            say(text, "en")
+    text = listen_and_recognize_whisper(r, source, "en", beep=True)
+    if text:
+        print("Spoken: ", text.lower())
+        say(text, "en")
 
+def recognize_command(r: sr.Recognizer, source):
+    print("Starting...")
+    r = setup_microphone()
+    text = listen_and_recognize_whisper(r, source, "en", beep=True)
+    print(text)
+    if "exit" in text.lower():
+        say("exiting", "en")
+        sys.exit()
+    if "talking" in text.lower():
+        say("enter your speech", "en")
+        recognize_once(r, source)
+
+def recognize_continuous(r: sr.Recognizer, source):
+    print("Starting...")
+    while True:
+        text = listen_and_recognize_whisper(r, source, "en")
+        if "buzz" in text.lower():
+            say("yes?", "en")
+            recognize_command(r, source)
+    
 if __name__ == '__main__':
-    main()
+    r = setup_microphone()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=1)
+        print("Adjusting...")
+        recognize_continuous(r, source)
+    open("temp.wav", "w").close()

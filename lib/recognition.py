6a17eb19b9eb86572e9dcb6ab30aff1466bdd762
Shaunak Warty <shaunak.warty@gmail.com>
2023-03-04 00:54:03 -0500

    Use op mega big brain state machine

--- lib/recognition.py
+++ lib/recognition.py
@@ -8,8 +8,11 @@ from pydub.playback import play
 
 r = sr.Recognizer()
 model = whisper.load_model("small.en")
+lang = "en"
 
-def transcribe(source: sr.AudioSource, from_code: str, beep=False):
+state = "listening"
+
+def transcribe(source: sr.AudioSource, beep=False):
     if beep:
         afile = AudioSegment.from_wav("assets/beep.wav")
         play(afile)
@@ -19,45 +22,48 @@ def transcribe(source: sr.AudioSource, from_code: str, beep=False):
         f.write(audio.get_wav_data())
         f.flush()
         print("Recognizing...")
-        result = model.transcribe(f.name, language=from_code)
+        result = model.transcribe(f.name)
         return result["text"]
 
-def say(text: str, to_code: str):
-    tts = gtts.gTTS(text, lang=to_code)
+def say(text: str):
+    tts = gtts.gTTS(text, lang=lang)
     fp = BytesIO()
     tts.write_to_fp(fp)
     fp.seek(0)
     aud = AudioSegment.from_file(fp, format="mp3")
     play(aud)
 
-def recognize_once(source: sr.AudioSource):
-    print("Starting...")
-    text = transcribe(source, "en", beep=True)
-    if text:
-        print("Spoken: ", text.lower())
-        say(text, "en")
+def handle_listening(text: str):
+    if "buzz" in text:
+        say("yes?")
+        return "command"
 
-def recognize_command(source: sr.AudioSource):
-    print("Starting...")
-    text = transcribe(source, "en", beep=True)
-    print(text)
-    if "exit" in text.lower():
-        say("exiting", "en")
+def handle_command(text: str):
+    if "exit" in text:
+        say("exiting")
         sys.exit()
-    if "talking" in text.lower():
-        say("enter your speech", "en")
-        recognize_once(source)
+    elif "talking" in text:
+        say("enter your speech")
+        return "speech"
+    return "command"
 
-def recognize_continuous(source: sr.AudioSource):
-    print("Starting...")
-    while True:
-        text = transcribe(source, "en")
-        if "buzz" in text.lower():
-            say("yes?", "en")
-            recognize_command(source)
+def handle_speech(text: str):
+    say(text)
+    return "listening"
     
 def start_loop():
     with sr.Microphone() as source:
         print("Adjusting...")
         r.adjust_for_ambient_noise(source, duration=1)
-        recognize_continuous(source)
+        
+        while True:
+            text = transcribe(source, "en").lower()
+            print("Heard:", text)
+
+            if text:
+                if state == "listening":
+                    state = handle_listening(text)
+                elif "command":
+                    state = handle_command(text)
+                elif state == "speech":
+                    state = handle_speech(text)

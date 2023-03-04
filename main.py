69b24245da90c8527a7686da31d9e6a7002deb8d
Ian B <ianboraks@gmail.com>
2023-03-04 00:38:02 -0500

    folder re-structuer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import speech_recognition as sr
 from dotenv import load_dotenv
 
-from recognition import start_loop
+from lib.recognition import start_loop
 
 if __name__ == "__main__":
     load_dotenv()

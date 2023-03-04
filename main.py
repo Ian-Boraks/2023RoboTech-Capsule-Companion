8b86304a22be21684e3f029c9bc8c5d8f5843d62
Abhinav Vemulapalli <nandan.av11@gmail.com>
2023-03-04 14:56:51 -0500

    Create task queue for the day

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import speech_recognition as sr
 from dotenv import load_dotenv
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import sleep
 
 from lib.chat import init_openai
@@ -9,16 +9,36 @@ from lib.recognition import init_recognizer, run_command
 from lib.config import setup_config, DAYS_OF_THE_WEEK
 from lib.communication import serial_write
 
-config = {}
+config = {
+    "saturday": {
+        "pills": [
+            ["12:00", "xanax"], 
+            ["13:00", "tylenol"],
+            ["12:00", "advil"],
+            ["13:00", "cocaine"],
+            ["11:00", "cocaine"]
+        ],
+        "therapist": "11:00",
+        "trainer": "15:00"
+    }
+}
 
 load_config_from_file = True
 
 today = None
-state = {
-    "pills": [],
-    "trainer": [],
-    "therapist": [],
-}
+queue = []
+
+def setup_tasks_for_day(day):
+    for task in day["pills"]:
+        temp = task.copy()
+        temp.insert(1, "pills")
+        queue.append(temp)
+    
+    queue.append([day["therapist"], "therapist"])
+    queue.append([day["trainer"], "trainer"])
+    queue.sort()
+    print(queue)
+    
 
 
 if __name__ == "__main__":
@@ -37,7 +57,7 @@ if __name__ == "__main__":
         print(config)
         
         while True:
-            command = True # check button press from serial
+            command = False # check button press from serial
             if command:
                 run_command(source)
 
@@ -46,8 +66,12 @@ if __name__ == "__main__":
             if now.date() != today:
                 day = config[DAYS_OF_THE_WEEK[now.weekday()]]
                 today = now.date()
-                state["pills"] = [False for i in day["pills"]]
-                state["therapist"] = [False for i in day["therapist"]]
-                state["trainer"] = [False for i in day["trainer"]]
+                print(f"Day: {day}")
+                setup_tasks_for_day(day)
+                print(f"Queue: {queue}")
 
             # logic
+            while len(queue) != 0:
+                current_task = queue.pop(0)
+                # perform based on current task
+

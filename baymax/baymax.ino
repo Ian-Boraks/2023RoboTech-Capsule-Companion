da7234a810458ee9a060c37077337bc6b808f176
Ian B <ianboraks@gmail.com>
2023-03-04 16:29:00 -0500

    Servo Setup

--- baymax/baymax.ino
+++ baymax/baymax.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 /*
 Designed by The Psychedelic Psychiatrist
 Robo-Tech 2023 @ GT
@@ -6,42 +8,78 @@ Robo-Tech 2023 @ GT
 #define bitRead(value, bit) (((value) >> (bit)) & 0b01)
 #define opCode(value) (((value) >> (6)))
 
-int[] servoPins   = {0, 1, 2, 3, 4, 5}
-int[] fAidPins    = {0, 0, 0, 0, 0, 0}
+#define DRAWER_DELAY 1000
 
-uint8_t command   = 0;
+Servo servos[] = {Servo(), Servo(), Servo(), Servo(), Servo(), Servo()};
 
-void setup() {
+int servoPins[] = {3, 5, 6, 9, 10, 11};
+int servoControl[] = {0, 0, 0, 0, 0, 0};
+
+uint8_t command = 0;
+
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  for (int i = 0; i < 6; i++)
+  {
+    servos[i].attach(servoPins[i]);
+    servos[i].write(0);
+  }
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  if(Serial.available() > 0) {
+void loop()
+{
+  if (Serial.available() > 0)
+  {
     command = Serial.read();
-    switch (opCode(command)))
+
+    switch (opCode(command))
     {
     case 1:
       /* Medicine */
-      for (int i = 0; i < 6; i++) {
-        analogWrite(servoPins[i], bitRead(command, i) * 255);
+      for (int i = 0; i < 6; i++)
+      {
+        if (bitRead(command, i) && !servoControl[i])
+        {
+          servoControl[i] = 1;
+          servos[i].write(90);
+          delay(10);
+        }
+        else if (!bitRead(command, i) && servoControl[i])
+        {
+          servoControl[i] = 0;
+          servos[i].write(0);
+          delay(10);
+        }
       }
       break;
     case 2:
       /* First Aid */
-      for (int i = 0; i < 6; i++) {
-        analogWrite(fAidPins[i], bitRead(command, i) * 255);
-      }
       break;
     case 3:
       /* Workout */
       break;
-    default:
-      /* Default State */
-      for (int i = 0; i < 7; i++) {
-        analogWrite(servoPins[i], 0);
+    case 0:
+      /* Reset State */
+      if (command != 0)
+        break;
+
+      for (int i = 0; i < 6; i++)
+      {
+        if (servoControl[i])
+        {
+          servoControl[i] = 0;
+          servos[i].write(0);
+        }
+        delay(10);
       }
+      break;
+    default:
       break;
     }
   }
+  delay(10);
 }

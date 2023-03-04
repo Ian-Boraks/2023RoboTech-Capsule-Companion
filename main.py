eefaacd2eec5765e4a2d050e61e28da782c60ab3
Ian B <ianboraks@gmail.com>
2023-03-04 00:46:52 -0500

    start of config

--- main.py
+++ main.py
@@ -3,6 +3,34 @@ from dotenv import load_dotenv
 
 from lib.recognition import start_loop
 
+pills = { }
+
+config = {
+    "PILLS": {
+        "MONDAY": [
+            0, []
+        ],
+        "TUESDAY": [
+            0, []
+        ],
+        "WEDNESDAY": [
+            0, []
+        ],
+        "THURSDAY": [
+            0, []
+        ],
+        "FRIDAY": [
+            0, []
+        ],
+        "SATURDAY": [
+            0, []
+        ],
+        "SUNDAY": [
+            0, []
+        ]
+    }
+}
+
 if __name__ == "__main__":
     load_dotenv()
     start_loop()

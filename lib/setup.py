4dec818af48da5468c164d55575a833391fe5089
Abhinav Vemulapalli <nandan.av11@gmail.com>
2023-03-04 01:10:50 -0500

    planning out for setup and main function

---
+++ lib/setup.py
@@ -0,0 +1,39 @@
+return {
+    "SCHEDULE" : {
+        "MONDAY" : {
+            "PILLS" : [[0, []]],
+            "TRAINER": [[0, []]],
+            "THERAPIST" : [[0]]
+        },
+        "TUESDAY" : {
+            "PILLS" : [[0, []]],
+            "TRAINER": [[0, []]],
+            "THERAPIST" : [[0]]
+        },
+        "WEDNESDAY" : {
+            "PILLS" : [[0, []]],
+            "TRAINER": [[0, []]],
+            "THERAPIST" : [[0]]
+        },
+        "THURSDAY" : {
+            "PILLS" : [[0, []]],
+            "TRAINER": [[0, []]],
+            "THERAPIST" : [[0]]
+        },
+        "FRIDAY" : {
+            "PILLS" : [[0, []]],
+            "TRAINER": [[0, []]],
+            "THERAPIST" : [[0]]
+        },
+        "SATURDAY" : {
+            "PILLS" : [[0, []]],
+            "TRAINER": [[0, []]],
+            "THERAPIST" : [[0]]
+        },
+        "SUNDAY" : {
+            "PILLS" : [[0, []]],
+            "TRAINER": [[0, []]],
+            "THERAPIST" : [[0]]
+        }
+    }
+}
--- main.py
+++ main.py
@@ -5,47 +5,9 @@ from lib.recognition import start_loop
 
 pills = { }
 
-day = []
+config = setup_week()
 
-config = {
-    "SCHEDULE" : {
-        "MONDAY" : {
-            "PILLS" : [[0, []]],
-            "TRAINER": [[0, []]],
-            "THERAPIST" : [[0]]
-        },
-        "TUESDAY" : {
-            "PILLS" : [[0, []]],
-            "TRAINER": [[0, []]],
-            "THERAPIST" : [[0]]
-        },
-        "WEDNESDAY" : {
-            "PILLS" : [[0, []]],
-            "TRAINER": [[0, []]],
-            "THERAPIST" : [[0]]
-        },
-        "THURSDAY" : {
-            "PILLS" : [[0, []]],
-            "TRAINER": [[0, []]],
-            "THERAPIST" : [[0]]
-        },
-        "FRIDAY" : {
-            "PILLS" : [[0, []]],
-            "TRAINER": [[0, []]],
-            "THERAPIST" : [[0]]
-        },
-        "SATURDAY" : {
-            "PILLS" : [[0, []]],
-            "TRAINER": [[0, []]],
-            "THERAPIST" : [[0]]
-        },
-        "SUNDAY" : {
-            "PILLS" : [[0, []]],
-            "TRAINER": [[0, []]],
-            "THERAPIST" : [[0]]
-        }
-    }
-}
+day = setup_day(day_of_week)
 
 if __name__ == "__main__":
     load_dotenv()

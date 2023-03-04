bec53fe39325d2be2290da7794207329fac237d6
Abhinav Vemulapalli <nandan.av11@gmail.com>
2023-03-04 15:16:42 -0500

    Added skeleton code to execute therapy session based on queue

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 import json
 import speech_recognition as sr
 from dotenv import load_dotenv
-from datetime import datetime, timedelta
-from time import sleep
+from datetime import datetime
 
 from lib.chat import init_openai
-from lib.recognition import init_recognizer, run_command
+from lib.recognition import init_recognizer, run_command, run_therapy
 from lib.config import setup_config, DAYS_OF_THE_WEEK
 from lib.communication import serial_write
 
-config = {
-    "saturday": {
-        "pills": [
-            ["12:00", "xanax"], 
-            ["13:00", "tylenol"],
-            ["12:00", "advil"],
-            ["13:00", "cocaine"],
-            ["11:00", "cocaine"]
-        ],
-        "therapist": "11:00",
-        "trainer": "15:00"
-    }
-}
+config = {}
 
 load_config_from_file = True
 
@@ -64,14 +51,20 @@ if __name__ == "__main__":
             now = datetime.now()
 
             if now.date() != today:
-                day = config[DAYS_OF_THE_WEEK[now.weekday()]]
+                day = config["schedule"][DAYS_OF_THE_WEEK[now.weekday()]]
                 today = now.date()
-                print(f"Day: {day}")
                 setup_tasks_for_day(day)
-                print(f"Queue: {queue}")
 
             # logic
             while len(queue) != 0:
-                current_task = queue.pop(0)
-                # perform based on current task
-
+                if queue[0][0] <= datetime.now().strftime("%H:%M"):
+                    current_task = queue.pop(0)
+                    if current_task[1] == "pills":
+                        
+                        continue
+                    elif current_task[1] == "therpist":
+                        run_therapy()
+                    elif current_task[2] == "trainer":
+                        continue
+                else:
+                    break

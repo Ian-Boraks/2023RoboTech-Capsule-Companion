f84f9a234ef3c3ba40e7ccd875aa94a5d9788787
Garrett Comes <gcomes3@gatech.edu>
2023-03-04 23:08:05 -0500

    Fixed Video Display

--- lib/video.py
+++ lib/video.py
@@ -1,62 +1,31 @@
-import tkinter as tk
 import cv2
-from PIL import Image, ImageTk
 
-def create_fullscreen_window():
-    # Initialize the tkinter window
-    root = tk.Tk()
+video = cv2.VideoCapture('wave.mp4')
 
-    # Set the window to fullscreen
-    root.attributes("-fullscreen", True)
+cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
 
-    # Hide the mouse cursor
-    root.config(cursor="none")
+while True:
+    screen_width, screen_height = 3024, 1964
+    ret, frame = video.read()
+    if frame is not None:
+        frame_height, frame_width, _ = frame.shape
+        scaleWidth = float(screen_width)/float(frame_width)
+        scaleHeight = float(screen_height)/float(frame_height)
 
-    # Create a canvas in the window to display content
-    canvas = tk.Canvas(root, width=root.winfo_screenwidth(), height=root.winfo_screenheight())
-    canvas.pack()
+        if scaleHeight>scaleWidth:
+            imgScale = scaleWidth
+        else:
+            imgScale = scaleHeight
 
-    # Set the video file path
-    video_path = "wave.mp4"
+        newX,newY = frame.shape[1]*imgScale, frame.shape[0]*imgScale
+        frame = cv2.resize(frame,(int(newX),int(newY)))
 
-    # Initialize the video capture object
-    cap = cv2.VideoCapture(video_path)
+    if ret:
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        video.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-    # Loop the video
-    def loop_video():
-        # Read a frame from the video
-        ret, frame = cap.read()
-
-        # Check if the frame was read successfully
-        if not ret:
-            # Restart the video if the end is reached
-            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-            return
-
-        # Convert the frame to RGB format and resize it to fit the canvas
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = cv2.resize(frame, (root.winfo_screenwidth(), root.winfo_screenheight()))
-
-        # Create a PIL image object from the frame
-        image = Image.fromarray(frame)
-
-        # Create a Tkinter-compatible photo image from the PIL image object
-        photo = ImageTk.PhotoImage(image=image)
-
-        # Display the photo on the canvas
-        canvas.create_image(0, 0, anchor=tk.NW, image=photo)
-
-        # Schedule the next frame update
-        root.after(1, loop_video)
-
-    # Start the video loop
-    loop_video()
-
-    # Return the tkinter window and canvas objects
-    return root, canvas
-
-if __name__ == "__main__":
-    root, canvas = create_fullscreen_window()
-
-    # Start the tkinter main loop
-    root.mainloop()
+video.release()
+cv2.destroyAllWindows()

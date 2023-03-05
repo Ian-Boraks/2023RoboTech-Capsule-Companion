1538f0cd770ad7638cd6536e55d73c4e514db38e
Garrett Comes <gcomes3@gatech.edu>
2023-03-05 03:10:53 -0500

    video stuff

--- lib/video.py
+++ lib/video.py
@@ -30,3 +30,9 @@ def play_video():
 
     video.release()
     cv2.destroyAllWindows()
+
+def init_video():
+    threading.Thread(target=play_video).start()
+
+if __name__ == "__main__":
+    play_video()
